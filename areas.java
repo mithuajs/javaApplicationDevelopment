554cd45c37a27984feed2503ca4b94717beb711e
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-14 20:00:49 +0600

    circle areas add naw java

---
+++ areas.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class  areas {
+    public static void main(String[] args) {
+        Scanner mithu = new Scanner(System.in);
+
+        double r,area;
+        System.out.print("Enter radius : "); 
+        r = mithu.nextDouble();
+
+        area = 3.14 * r * r;
+        System.out.println("Area of circle is : " + area);
+    }
+}
