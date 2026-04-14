5237dd3de0fc4e4ba251f70787315e0d8ca557de
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-14 14:19:47 +0600

    first commit

---
+++ SumExample.java
@@ -0,0 +1,8 @@
+public class SumExample {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
