f2d1aaefb3f65e507c99b9e26c71ab45c3814709
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-22 12:22:52 +0600

    1 to 100 sum add program and user input add same fill

---
+++ Mithu.java
@@ -0,0 +1,37 @@
+//  user thaki input nibo and sum of 1 to n print korbo.
+
+// import java.util.Scanner;
+
+// public class  Mithu {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+
+//         int sum = 0;
+//         for(int i = 1; i <= n; i++) {
+//             sum += i;
+//         }
+
+//         System.out.println("Sum = " + sum);
+//     }
+    
+// }
+
+
+
+public class Mithu {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
+// output
+// Sum = 5050
